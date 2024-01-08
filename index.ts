ea32bb64da2088fea796c68474b3cbdd3448d24d
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2024-01-07 19:38:56 -0700

    Make main script more complex

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
+import MathWorker from './src/workers.js'
+import { randomInt } from 'crypto';
+
 console.log('Starting work.');
+
+const myMw = new MathWorker(10);
+
+while (myMw.getTokens() > 0) {
+  console.log(`Tokens remaining: ${myMw.getTokens()}`);
+  let x = randomInt(0, 100);
+  let y = randomInt(0, 100);
+
+  console.log(`${x} + ${y} = ${myMw.add(x, y)}`);
+}
